de37f7920b8b0dd39a4fec8d558aabd3078e3c8f
unknown <FALCAO@FALCAO.quidgest.mz>
2025-04-08 16:38:04 +0200

    first commit

---
+++ Controllers/ProdutoController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class ProdutoController : Controller
+    {
+        private readonly ILogger<ProdutoController> _logger;
+        public ProdutoController(ILogger<ProdutoController> logger)
+        {
+            _logger = logger;
+        }
+        public ActionResult Index()
+        {
+            // Exemplo com uma lista de produtos fictícia
+            var produtos = new List<Produto>
+        {
+            new Produto { Id = 1, Nome = "Camiseta", Preco = 29.90m },
+            new Produto { Id = 2, Nome = "Calça", Preco = 79.90m },
+            new Produto { Id = 3, Nome = "Tênis", Preco = 150.00m }
+        };
+
+            return View(produtos);
+        }
+    }
+
+}
---
+++ Models/Fornecedor.cs
@@ -0,0 +1,9 @@
+﻿namespace WebApplication1.Models
+{
+    public class Fornecedor
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public decimal email { get; set; }
+    }
+}
